d310a2211beef1d1cc682610dd5e448fbef25742
sachindevxts <spathak@xtsworld.in>
2026-07-20 19:21:32 +0530

    update Vite configuration to include envDir and reorganize settings

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      },
+    envDir: 'env',
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    css: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        css: true,
     },
-  },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
 });
